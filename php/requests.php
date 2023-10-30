3b12dda5b76c03589f75b5a08b2850c80d610e07
John <134572447+Kawasaaki@users.noreply.github.com>
2023-10-30 20:21:10 +0545

    Update requests.php

--- php/requests.php
+++ php/requests.php
@@ -1,4 +1,18 @@
-﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
-; #Warn  ; Enable warnings to assist with detecting common errors.
-SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
-SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = 'dulaljohn80@gmail.com';
+    $subject = 'Contact Form Submission';
+    $headers = 'From: ' . $email;
+    $message_body = "Name: $name\nEmail: $email\nMessage:\n$message";
+
+    if (mail($to, $subject, $message_body, $headers)) {
+        echo '<p>Thank you for your message. We will get back to you shortly.</p>';
+    } else {
+        echo '<p>Sorry, there was an error sending your message. Please try again later.</p>';
+    }
+}
+?>
